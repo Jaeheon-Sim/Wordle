fcf01db05dcd8b6adb610c62fc0695e446dacee8
Jaeheon-Sim <zh025700@naver.com>
2022-03-04 00:48:39 +0900

    0304

--- js/btn.js
+++ js/btn.js
@@ -1,22 +1,28 @@
 var inputs = document.querySelectorAll(".plus");
 const typeEnter = document.querySelector("#enter");
 const answer = "hello";
+let count = 0;
 
-typeEnter.addEventListener('click',checkAnswer);
+typeEnter.addEventListener("click", checkAnswer);
 
-function checkAnswer(event){
-    event.preventDefault();
-    inputs = document.querySelectorAll(".plus");
-   for(let i=0;i<5;i++){
-        if(inputs[i].value == answer[i]){
-            inputs[i].style.background ='green';
-        }else if(answer.includes(inputs[i].value)){
-            inputs[i].style.background ='yellow';
-        }else{
-            inputs[i].style.background ='red';
-        }
-        inputs[i].classList.remove('plus');
-   }
-   // 다 초록색이면 끝내
-   // 아니면 다음 div에 클래스 추가
+function checkAnswer(event) {
+  event.preventDefault();
+  inputs = document.querySelectorAll(".plus");
+  for (let i = 0; i < 5; i++) {
+    if (inputs[i].value == answer[i]) {
+      inputs[i].style.background = "green";
+      count++;
+    } else if (answer.includes(inputs[i].value)) {
+      inputs[i].style.background = "yellow";
+    } else {
+      inputs[i].style.background = "red";
+    }
+    inputs[i].classList.remove("plus");
+    inputs[i].disabled = true;
+  }
+  count = 0;
+}
+// 아니면 다음 div에 클래스 추가
+if (count == 5) {
+  alert("finish!!!!!!!!!!!!!!");
 }
